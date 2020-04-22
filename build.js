f8ae960ebdba51bb99dc7a0849f99dbd34e54cfb
ari <sndaritsune@gmail.com>
2020-04-22 17:30:50 -0300

    add TOC generation

--- build.js
+++ build.js
@@ -1,22 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
 const { Remarkable } = require('remarkable');
-const fs = require('fs')
-const path = require('path')
+const toc = require('markdown-toc');
 
 const OUT_DIR = 'dist'
 
-const md = new Remarkable({
+
+
+const md = () => new Remarkable({
   html: true
 });
 
 
 // FAQ
 const faqInput = fs.readFileSync('./index.md', 'utf-8')
-const faqOutput = md.render(faqInput)
+const faqTocMarkdown = md().use(toc.plugin()).render(faqInput).content
+const faqOutput = md().render(
+  faqInput.replace('<!-- toc -->', faqTocMarkdown)
+)
 
 // Create dist directory if it doesn't exist & save output to it
 if (!fs.existsSync(OUT_DIR)) {
   fs.mkdirSync(OUT_DIR);
 }
 
-const faqOutputPath = path.join('.', OUT_DIR, 'index.html')
-fs.writeFileSync(faqOutputPath, faqOutput, 'utf-8')
+const faqOutputPath = path.join('.', OUT_DIR, 'index.html');
+fs.writeFileSync(faqOutputPath, faqOutput, 'utf-8');
