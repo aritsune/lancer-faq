1b144b0c40bb25c86e1a046e7dc38ee47a66fc38
ari <sndaritsune@gmail.com>
2020-04-22 18:09:18 -0300

    tweak build process, add style

--- build.js
+++ build.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { Remarkable } = require('remarkable');
 const toc = require('markdown-toc');
 
+const SRC_DIR = 'src'
 const OUT_DIR = 'dist'
 
 
@@ -14,16 +15,34 @@ const md = () => new Remarkable({
 
 
 // FAQ
-const faqInput = fs.readFileSync('./index.md', 'utf-8')
-const faqTocMarkdown = md().use(toc.plugin()).render(faqInput).content
-const faqOutput = md().render(
-  faqInput.replace('<!-- toc -->', faqTocMarkdown)
-)
+const faqInput = fs.readFileSync('./src/index.md', 'utf-8')
+const faqTocMarkdown = md().use(toc.plugin()).render(faqInput)
+
+
+
+let faqOutput = md()
+  .use(function (remarkable) {
+    remarkable.renderer.rules.heading_open = function (tokens, idx) {
+      if (idx === 0) return '<h' + tokens[idx].hLevel + '>';
+      return '<h' + tokens[idx].hLevel + ' id=' + toc.slugify(tokens[idx + 1].content) + '>'
+    };
+    remarkable.renderer.rules.heading_close = function (tokens, idx) {
+      if (idx === 2) return '</h' + tokens[idx].hLevel + '>';
+      return '<span class="permalink">[<a href="#' + toc.slugify(tokens[idx - 1].content) + '">link</a>]</span>' + '</h' + tokens[idx].hLevel + '>';
+    };
+  })
+  .render(
+    faqInput.replace('<!-- toc -->', faqTocMarkdown.content)
+  )
+
+faqOutput += '\n\n<link rel="stylesheet" href="./style.css">'
 
 // Create dist directory if it doesn't exist & save output to it
 if (!fs.existsSync(OUT_DIR)) {
   fs.mkdirSync(OUT_DIR);
 }
 
+
+fs.copyFileSync('./src/style.css', path.join('.', OUT_DIR, 'style.css'))
 const faqOutputPath = path.join('.', OUT_DIR, 'index.html');
 fs.writeFileSync(faqOutputPath, faqOutput, 'utf-8');
